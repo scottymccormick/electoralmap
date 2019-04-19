2cea570cd1ffd6c65a38b232fd63ba270dec9ada
Scott McCormick <scott.t.mccormick@gmail.com>
2019-04-18 18:28:10 -0700

    load map of us at 960 x 600

--- src/USMap.js
+++ src/USMap.js
@@ -6,13 +6,29 @@ class USMap extends Component {
   loadMap() {
     d3.json('https://cdn.jsdelivr.net/npm/us-atlas@2/us/states-10m.json')
       .then(mapData => {
-        console.log(mapData)
         const geoData = topojsonFeature(mapData, mapData.objects.states).features
-        console.log(geoData)
+        this.displayMap(geoData)
       })
       .catch(err => {
         throw err
       })
+  }
+  displayMap(data) {
+    const width = 960
+    const height = 600
+    
+    const path = d3.geoPath()
+    console.log(path)
+
+    d3.select('svg')
+          .attr('width', width)
+          .attr('height', height)
+        .selectAll('.states')
+        .data(data)
+        .enter()
+          .append('path')
+          .classed('state', true)
+          .attr('d', path)
   }
   componentDidMount() {
     this.loadMap()
@@ -22,7 +38,10 @@ class USMap extends Component {
     return (
       <div>
         <h3>US Map</h3>
-
+        <svg 
+          version="1.1"
+          baseProfile="full"
+          xmlns="http://www.w3.org/2000/svg"></svg>
       </div>
     )
   }
