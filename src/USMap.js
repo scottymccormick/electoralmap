727aace97ffd6dab1033137fffb2d3e6917cf0c3
Scott McCormick <scott.t.mccormick@gmail.com>
2019-04-18 17:41:53 -0700

    add custom favicons

--- src/USMap.js
+++ src/USMap.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import * as d3 from 'd3'
 
 class USMap extends Component {
   render() {
+    console.log(d3)
     return (
-      <h3>US Map</h3>
+      <div>
+        <h3>US Map</h3>
+
+      </div>
     )
   }
 }
