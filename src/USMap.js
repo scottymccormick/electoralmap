298ea74acd942b11d65f14c0d0166b40e7302121
Scott McCormick <scott.t.mccormick@gmail.com>
2019-04-18 17:54:34 -0700

    convert topojson to geojson data.

--- src/USMap.js
+++ src/USMap.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import {feature as topojsonFeature} from 'topojson'
 import * as d3 from 'd3'
 
 class USMap extends Component {
   loadMap() {
     d3.json('https://cdn.jsdelivr.net/npm/us-atlas@2/us/states-10m.json')
-      .then(data => {
-        console.log(data)
+      .then(mapData => {
+        console.log(mapData)
+        const geoData = topojsonFeature(mapData, mapData.objects.states).features
+        console.log(geoData)
       })
       .catch(err => {
         throw err
