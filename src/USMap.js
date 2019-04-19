18a921a0115bca5d86045389cb6eb473a7ca39fe
Scott McCormick <scott.t.mccormick@gmail.com>
2019-04-18 17:49:36 -0700

    load in topology data.

--- src/USMap.js
+++ src/USMap.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 import * as d3 from 'd3'
 
 class USMap extends Component {
+  loadMap() {
+    d3.json('https://cdn.jsdelivr.net/npm/us-atlas@2/us/states-10m.json')
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+        throw err
+      })
+  }
+  componentDidMount() {
+    this.loadMap()
+  }
   render() {
     console.log(d3)
     return (
