22b7c40e34ce184aff1922a32017f229be8b84c0
Scott McCormick <scott.t.mccormick@gmail.com>
2019-04-23 16:47:56 -0700

    add temporary styling to make labels visible on input range.

--- src/YearPicker.js
+++ src/YearPicker.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const YearPicker = props => {
+  const years = []
+  for (let year = 2000; year <= 2016; year += 2) {
+    years.push(year)
+  }
   return (
     <section>
-      <input type="range" name="year" id="year-slider" min="1790" max="2016" step="2" onChange={props.handleChange}/>
+      <input type="range" list="tickmarks" name="year" id="year-slider" onChange={props.handleChange} min="2000" max="2016" step="2" defaultValue="2016"/>
+      <datalist id="tickmarks">
+        { years.map(year => {
+          return <option value={year} label={year} key={year} />
+        })}
+      </datalist>
     </section>
   )
 }
