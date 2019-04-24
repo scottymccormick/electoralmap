25da529c6477ee92ed0479cab207c90a7947e2ec
Scott McCormick <scott.t.mccormick@gmail.com>
2019-04-23 18:02:07 -0700

    remove blank columns, add d3 csv formatters, calculate strength scores.

--- src/USMap.js
+++ src/USMap.js
@@ -219,11 +219,43 @@ class USMap extends Component {
       collegeData: []
     }
   }
+  censusFormatter = row => {
+    const year = row['Year']
+    const result = {
+      year: +year,
+      states: []
+    }
+    for (const key in row) {
+      if (key !== 'Year') {
+        result.states.push({
+          name: key,
+          population: +row[key]
+        })
+      }
+    }
+    return result
+  }
+  collegeFormatter = row => {
+    const year = row['Year']
+    const result = {
+      year: +year,
+      states: []
+    }
+    for (const key in row) {
+      if (key !== 'Year') {
+        result.states.push({
+          name: key,
+          votes: +row[key]
+        })
+      }
+    }
+    return result
+  }
   loadData() {
 
     const promises = []
-    promises.push(d3.csv(`${process.env.PUBLIC_URL}/data/collegebyyear.csv`))
-    promises.push(d3.csv(`${process.env.PUBLIC_URL}/data/censusbyyear.csv`))
+    promises.push(d3.csv(`${process.env.PUBLIC_URL}/data/collegebyyear.csv`, this.collegeFormatter))
+    promises.push(d3.csv(`${process.env.PUBLIC_URL}/data/censusbyyear.csv`, this.censusFormatter))
 
     Promise.all(promises)
       .then(([collegeData, censusData]) => {
@@ -237,17 +269,13 @@ class USMap extends Component {
       })
   }
   getData() {
-    const collegeYear = this.state.collegeData.find(row => {
-      const year = +row["Year"]
-
-      return year === this.state.year || 
-        (Math.abs(year - this.state.year) <= 2 && year < this.state.year)
+    const collegeYear = this.state.collegeData.find(entry => {
+      return entry.year === this.state.year || 
+        (Math.abs(entry.year - this.state.year) <= 2 && entry.year < this.state.year)
     })
 
-    const censusYear = this.state.censusData.find(row => {
-      const year = +row["Year"]
-
-      return year === this.state.year
+    const censusYear = this.state.censusData.find(entry => {
+      return entry.year === this.state.year
     })
     return [collegeYear, censusYear]
   }
@@ -255,7 +283,7 @@ class USMap extends Component {
     d3.json('https://cdn.jsdelivr.net/npm/us-atlas@2/us/states-10m.json')
       .then(mapData => {
         const geoDataObj = topojsonFeature(mapData, mapData.objects.states)
-        console.log(geoDataObj)
+        
         testStateData.forEach(row => {
           const states =  geoDataObj.features.filter(d => d.properties.name === row.name)
           states.forEach(state => state.properties = row)
@@ -290,10 +318,33 @@ class USMap extends Component {
       this.setColor()
     }, 1000)
   }
-  setColor() {
+  getScores(collegeYear, censusYear) {
+    const strengthScores = []
+    let popTotal = 0
+    censusYear.states.forEach(state => {
+      popTotal += state.population
+    })
+    let voteTotal = 0
+    collegeYear.states.forEach(state => {
+      voteTotal += state.votes
+    })
 
-    const [ collegeYear, censusYear ] = this.getData()
-    console.log(collegeYear, censusYear)
+    for (let i = 0; i < censusYear.states.length; i++) {
+      const stateCensus = censusYear.states[i];
+      const stateCollege = collegeYear.states.find(state => state.name === stateCensus.name)
+      const score = (stateCollege.votes / voteTotal) / (stateCensus.population / popTotal)
+      strengthScores.push({
+        name: stateCensus.name,
+        score
+      })
+    }
+    return strengthScores
+  }
+  async setColor () {
+
+    const [ collegeYear, censusYear ] =  await this.getData()
+
+    const strengthScores = this.getScores(collegeYear, censusYear)
 
     const colorRange = ['white', 'blue']
 
