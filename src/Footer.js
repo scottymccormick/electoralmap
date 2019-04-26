4436d487341c57649678bbe937455eddfe3b1d7f
Scott McCormick <scott.t.mccormick@gmail.com>
2019-04-26 15:53:54 -0700

    Center country map, add linkedin and github link hrefs.

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <h2>Contact</h2>
       <p className="aboutme">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, aspernatur animi in atque dicta voluptas totam enim voluptatem veniam autem adipisci deserunt ipsum molestiae beatae laudantium laborum reiciendis vitae minima culpa pariatur ab iste odit eos dolore. Recusandae tempore aperiam, veritatis, ipsam deleniti modi id quia, perferendis quod soluta totam!</p>
       <p>
-        <a href="#">LinkedIn</a> | <a href="#">GitHub</a>
+        <a href="https://linkedin.com/in/scottymccormick">LinkedIn</a> | <a href="https://github.com/scottymccormick">GitHub</a>
       </p>
     </section>
   )
