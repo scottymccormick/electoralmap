1a6d10f7be0ee03326d69b02f4e843e04691069a
Scott McCormick <scott.t.mccormick@gmail.com>
2019-05-05 14:49:56 -0700

    Update contact section.

--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,11 @@ const Footer = () => {
   return (
     <section className="footer-section">
       <h2>Contact</h2>
-      <p className="aboutme">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, aspernatur animi in atque dicta voluptas totam enim voluptatem veniam autem adipisci deserunt ipsum molestiae beatae laudantium laborum reiciendis vitae minima culpa pariatur ab iste odit eos dolore. Recusandae tempore aperiam, veritatis, ipsam deleniti modi id quia, perferendis quod soluta totam!</p>
-      <p>
-        <a href="https://linkedin.com/in/scottymccormick">LinkedIn</a> | <a href="https://github.com/scottymccormick">GitHub</a>
-      </p>
+      <div className="aboutme">
+        <a href="https://linkedin.com/in/scottymccormick" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://github.com/scottymccormick" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://scottymccormick.me/" target="_blank" rel="noopener noreferrer">Portfolio</a>
+      </div>
     </section>
   )
 }
